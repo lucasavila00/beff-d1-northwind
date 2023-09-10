4c72c1690cb2386f3380fa47261c3c49191b5f0d
Lucas Avila <lucaxx@gmail.com>
2023-09-10 20:30:24 -0300

    Add search

--- worker/lib/router.ts
+++ worker/lib/router.ts
@@ -319,4 +319,31 @@ export default {
       };
     },
   },
+  "/search": {
+    get: async (c: Ctx, q: string, table: string) => {
+      const itemsPerPage = 50;
+      const [stmts, sql] = prepareStatements(
+        c.hono.env.DB,
+        false,
+        [
+          table == "products"
+            ? "SELECT Id, ProductName, SupplierId, CategoryId, QuantityPerUnit, UnitPrice, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued FROM Product WHERE ProductName LIKE ?2 LIMIT ?1"
+            : "SELECT Id, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax FROM Customer WHERE CompanyName LIKE ?2 OR ContactName LIKE ?2 OR ContactTitle LIKE ?2 OR Address LIKE ?2 LIMIT ?1",
+        ],
+        [[itemsPerPage, `%${q}%`]]
+      );
+      const search = await (stmts[0] as D1PreparedStatement).all();
+      return {
+        items: itemsPerPage,
+        stats: {
+          queries: 1,
+          results: search.results ? search.results.length : 0,
+          select_fts: 0,
+          select_where: 1,
+          log: createSQLLog(sql, [search]),
+        },
+        results: search.results,
+      };
+    },
+  },
 };
